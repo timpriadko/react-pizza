1d9bfeb004e09ba3ebfb2f70592cf99469170dfa
unknown <timpriadko@gmail.com>
2020-06-28 00:55:10 +0300

    index layout to react layout; make header and button components;

---
+++ src/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const Button = (props) => {
+    console.log(props)
+    return (
+        <button className={classNames('button',
+            {
+                'button-outline': props.outline,
+            }
+        )}>
+            {props.children}
+        </button>
+    )
+}
+
+export default Button;
