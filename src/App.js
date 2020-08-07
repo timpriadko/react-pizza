187b7b7d3537faa667b251b0be1bb5c14b5643fe
unknown <timpriadko@gmail.com>
2020-08-07 10:20:05 +0300

    App component-change from class to functional

--- src/App.js
+++ src/App.js
@@ -1,63 +1,38 @@
 import React from 'react';
 import axios from 'axios';
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
 import { setPizzas } from './redux/actions/pizzas';
 
-// function App() {
-//   // const [pizzas, setPizzas] = useState([]);
+function App({ items }) {
+  const dispatch = useDispatch();
 
-//   React.useEffect(() => {
-//     axios.get('http://localhost:3000/db.json').then(({ data }) => {
-//       setPizzas(data.pizzas)
-//     })
-//   }, []);
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      dispatch(setPizzas(data.pizzas));
+    })
+  }, []);
 
-//   return (
-//     <div className="wrapper">
-//       <Header />
-//       <div className="content">
-//         <Route path="/" render={() => <Home items={pizzas} />} exact />
-//         <Route path="/cart" component={Cart} exact />
-//       </div>
-//     </div>
-//   );
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path="/" render={() => <Home items={items} />} exact />
+        <Route path="/cart" component={Cart} exact />
+      </div>
+    </div>
+  )
+}
+
+// (state) => {
+//   console.log(state)
+//   return {
+//     items: state.pizzas.items,
+//     filters: state.filters,
+//   }
 // }
 
-class App extends React.Component {
-  componentDidMount() {
-    axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      this.props.setPizzas(data.pizzas)
-    })
-  }
-  render() {
-    console.log(this.props)
-    return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Route path="/" render={() => <Home items={this.props.items} />} exact />
-          <Route path="/cart" component={Cart} exact />
-        </div>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    items: state.pizzas.items,
-    filters: state.filters,
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setPizzas: (items) => dispatch(setPizzas(items))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
