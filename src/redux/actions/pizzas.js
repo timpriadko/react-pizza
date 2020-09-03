d875a5020b07e5841d4c84b8b83ec86b5c523c93
unknown <timpriadko@gmail.com>
2020-09-03 20:42:38 +0300

    CartItem component; CartItem reducer;

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -7,7 +7,7 @@ export const setLoaded = (payload) => ({
 
 export const fetchPizzas = (sortBy, category) => (dispatch) => {
     dispatch(setLoaded(false));
-    axios.get(`http://localhost:3001/pizzas?${
+    axios.get(`/pizzas?${
         category !== null ? `category=${category}` : ''
         }&_sort=${sortBy.type}&_order=${sortBy.order}`).then(({ data }) => {
             dispatch(setPizzas(data));
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -4,20 +4,29 @@ const intialState = {
     totalCount: 0,
 }
 
+const getTotalPrice = (arr) => arr.reduce((sum, obj) => obj.price + sum, 0);
+
 const cart = (state = intialState, action) => {
     switch (action.type) {
         case 'ADD_PIZZA_CART': {
+            const currentPizzaItems = !state.items[action.payload.id]
+                ? [action.payload]
+                : [...state.items[action.payload.id].items,
+                action.payload];
+
+            console.log(currentPizzaItems)
+
             const newItems = {
                 ...state.items,
-                [action.payload.id]:
-                    !state.items[action.payload.id]
-                        ? [action.payload]
-                        : [...state.items[action.payload.id],
-                        action.payload]
+                [action.payload.id]: {
+                    items: currentPizzaItems,
+                    totalPrice: getTotalPrice(currentPizzaItems)
+                }
             };
 
-            const allPizzas = [].concat.apply([], Object.values(newItems));
-            const totalPrice = allPizzas.reduce((sum, obj) => obj.price + sum, 0);
+            const items = Object.values(newItems).map((obj) => obj.items);
+            const allPizzas = [].concat.apply([], items);
+            const totalPrice = getTotalPrice(allPizzas);
 
             return {
                 ...state,
@@ -26,6 +35,13 @@ const cart = (state = intialState, action) => {
                 totalPrice
             }
         }
+
+        case 'CLEAR_CART':
+            return {
+                items: {},
+                totalPrice: 0,
+                totalCount: 0,
+            }
 
         default:
             return state;
