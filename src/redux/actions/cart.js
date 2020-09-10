06ab03bb95b4da37bc6f48c8e9f47757aa9e261a
unknown <timpriadko@gmail.com>
2020-09-11 02:39:16 +0300

    finished cart funcs and reducers

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -1,8 +1,23 @@
 export const addPizzaToCart = (pizzaObj) => ({
-    type: 'ADD_PIZZA_CART',
-    payload: pizzaObj
-})
+  type: 'ADD_PIZZA_CART',
+  payload: pizzaObj,
+});
 
 export const clearCart = () => ({
-    type: 'CLEAR_CART'
-})
+  type: 'CLEAR_CART',
+});
+
+export const removeCartItem = (id) => ({
+  type: 'REMOVE_CART_ITEM',
+  payload: id,
+});
+
+export const plusCartItem = (id) => ({
+  type: 'PLUS_CART_ITEM',
+  payload: id,
+});
+
+export const minusCartItem = (id) => ({
+  type: 'MINUS_CART_ITEM',
+  payload: id,
+});
